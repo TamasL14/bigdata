3e882b488734ab388ec4de6d86925bde909afeae
TamasL14 <lukacstamas1114@gmail.com>
2024-02-05 08:12:04 +0100

    Update

--- app/main.py
+++ app/main.py
@@ -7,42 +7,6 @@ app = FastAPI()
 async def startup_event():
     await crud.connect_to_mongo()
 
-@app.get("/")
-async def get_data():
-    return await crud.get_data()
-
-@app.post("/")
-async def post_data():
-    return await crud.post_data()
-
-@app.delete("/")
-async def delete_data():
-    return await crud.delete_data()
-
-@app.put("/")
-async def update_data():
-    return await crud.update_data()
-
-@app.patch("/")
-async def patch_data():
-    return await crud.patch_data()
-
-@app.get("/{id}")
-async def get_data_by_id(id: str):
-    return await crud.get_data_by_id(id)
-
-@app.post("/{id}")
-async def post_data_by_id(id: str):
-    return await crud.post_data_by_id(id)
-
-@app.delete("/{id}")
-async def delete_data_by_id(id: str):
-    return await crud.delete_data_by_id(id)
-
-@app.put("/{id}")
-async def update_data_by_id(id: str):
-    return await crud.update_data_by_id(id)
-
 @app.get("/health")
 async def health_check():
     return await crud.health_check()
