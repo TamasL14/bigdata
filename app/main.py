de74a2578a8b307bd4f51241208b18c8b41a335a
TamasL14 <lukacstamas1114@gmail.com>
2024-02-05 13:46:19 +0100

    Update health_check function to include access credentials in the error message

--- app/main.py
+++ app/main.py
@@ -45,7 +45,7 @@ async def health_check():
         collections = db.list_collection_names()
         return {"message": collections}
     except Exception as e:
-        return {"message": "Connection failed: {}".format(e)}
+        return {"message": "Connection failed: {}".format(e), "Access": "${DB_USERNAME}, ${DB_PASSWORD}"}
     
 def is_folder(filename):
     # Use pathlib library to detect folder
