e39bd7b91c8426fed34dadf96ddc8e269fa0dfa7
TamasL14 <lukacstamas1114@gmail.com>
2024-02-05 08:19:43 +0100

    Update

--- app/main.py
+++ app/main.py
@@ -1,13 +1,29 @@
 from fastapi import FastAPI
-from app.database import crud
+from pymongo import MongoClient
+import os
+from dotenv import load_dotenv
 
 app = FastAPI()
+load_dotenv()
 
-@app.lifespan("startup")
-async def startup_event():
-    await crud.connect_to_mongo()
+DB_USERNAME = os.getenv('DB_USERNAME')
+DB_PASSWORD = os.getenv('DB_PASSWORD')
+MONGO_URL = "mongodb+srv://${DB_USERNAME}:${DB_PASSWORD}@rosentestdata.ky0vl7x.mongodb.net/?retryWrites=true&w=majority+&ssl=true&ssl_cert_reqs=CERT_NONE"
+
+@app.get("/")
+async def connect():
+    try:
+        # Connect to MongoDB
+        client = MongoClient(MONGO_URL)
+        return {"message": "Connection successful"}
+    except Exception as e:
+        return {"message": "Connection failed: {}".format(e)}
 
 @app.get("/health")
 async def health_check():
-    return await crud.health_check()
+    try:
+        client = MongoClient(MONGO_URL)
+        return {"message": "Connection successful"}
+    except Exception as e:
+        return {"message": "Connection failed: {}".format(e)}
 
